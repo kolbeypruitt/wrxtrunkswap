c3f6c18187eacb3bd40ce8abcfce6a620083c073
kolbeypruitt <kolbeypruitt@gmail.com>
2016-03-23 18:31:07 -0600

    added catch all server route

--- server/server.js
+++ server/server.js
@@ -605,6 +605,10 @@ app.post('/auth/unlink', ensureAuthenticated, function(req, res) {
   });
 });
 
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/../client/index.html'))
+})
+
 /*
  |--------------------------------------------------------------------------
  | Start the Server
