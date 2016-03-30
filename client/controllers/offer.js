6783104e4a5ade0720b7b699f72f623b45473a56
kolbeypruitt <kolbeypruitt@gmail.com>
2016-03-29 19:51:05 -0600

    back to /

--- client/controllers/offer.js
+++ client/controllers/offer.js
@@ -1,6 +1,5 @@
 angular.module('MyApp')
   .controller('OfferCtrl', function ($scope, $http, $location, $rootScope, NgMap, toastr, $auth) {
-    var PORT = 8000;
     $scope.offer = {};
     $scope.years = [2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016];
     $scope.model = 'wrx';
