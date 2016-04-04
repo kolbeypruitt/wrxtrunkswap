6550871fd304a87e4fa878c06ece048ecf976fb9
kolbeypruitt <kolbeypruitt@gmail.com>
2016-04-04 16:20:58 -0600

    fixed the validations for offer page 1

--- client/controllers/offer.js
+++ client/controllers/offer.js
@@ -9,6 +9,9 @@ angular.module('MyApp')
       if ($scope.offer.year >= 2013) {
         $scope.offer.possiblePTS = true;
       }
+      if ($scope.offer.year <= 2012) {
+        $scope.offer.possiblePTS = false;
+      }
       if ($scope.offer.color) {
         $scope.offer.color = null;
       }
@@ -39,6 +42,9 @@ angular.module('MyApp')
     }
 
     $scope.nextOfferPage = function () {
+      $('button[type=submit]').click(function(){
+          $(this).attr('disabled', 'disabled');
+      });
       if ($scope.offer.year && $scope.offer.trim && $scope.offer.color && $scope.offer1current) {
         $scope.offer1current = false;
         $scope.offer2current = true;
@@ -46,6 +52,7 @@ angular.module('MyApp')
         $scope.offer2current = false;
         $scope.offer3current = true;
       }
+
     }
 
     $scope.prevOfferPage = function () {
@@ -67,35 +74,4 @@ angular.module('MyApp')
         })
       }
     }
-
-
-    // // file uploading..
-    // var vm = this;
-    // vm.submit = function(){ //function to call on form submit
-    //     if (vm.upload_form.file.$valid && vm.file) { //check if from is valid
-    //         vm.upload(vm.file); //call upload function
-    //     }
-    // }
-    
-    // vm.upload = function (file) {
-    //     Upload.upload({
-    //         url: 'http://localhost:3000/upload', //webAPI exposed to upload the file
-    //         data:{file:file} //pass file as data, should be user ng-model
-    //     }).then(function (resp) { //upload function returns a promise
-    //         if(resp.data.error_code === 0){ //validate success
-    //             $window.alert('Success ' + resp.config.data.file.name + 'uploaded. Response: ');
-    //         } else {
-    //             $window.alert('an error occured');
-    //         }
-    //     }, function (resp) { //catch error
-    //         console.log('Error status: ' + resp.status);
-    //         $window.alert('Error status: ' + resp.status);
-    //     }, function (evt) { 
-    //         console.log(evt);
-    //         var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-    //         console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
-    //         vm.progress = 'progress: ' + progressPercentage + '% '; // capture upload progress
-    //     });
-    // };
-
   });
